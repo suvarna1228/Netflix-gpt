f306939e9eb846fdde429eb18bf0c16fb3913b8d
suvarna1405 <suvarna7404@gmail.com>
2025-03-05 03:51:36 +0530

    project

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,9 @@
 import React, { useRef, useState } from 'react'
 import Header from './Header';
 import { checkValidData } from '../utils/validate';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../utils/firebase';
+
 
 const Login = () => {
   const [isSignInForm,setIsSignInForm] = useState(true);
@@ -8,32 +11,59 @@ const Login = () => {
   const [errorMessage,setErrorMessage] = useState(null);
 
   const [showMore, setShowMore] = useState(false);
-
+  const name = useRef(null);
   const email=useRef(null);
   const password=useRef(null);
 
   const handleButtonClick = ()=>{
-    console.log(email.current.value);
-    console.log(password.current.value);
+
     const message = checkValidData(email.current.value, password.current.value);
     setErrorMessage(message);
+
+    if(message) return;
+    if(!isSignInForm){
+      createUserWithEmailAndPassword(
+        auth, 
+        email.current.value, 
+        password.current.value
+      )
+      .then((userCredential) => {
+        
+        const user = userCredential.user;
+        console.log(user);
+        
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setErrorMessage(errorCode+"-"+errorMessage);
+      });
+    
+    }
+    else{
+
+    }
+
   };
   const  toggleSignInForm =()=>{
        setIsSignInForm(!isSignInForm);
   };
   return (
-  <div >
+  <div className="relative min-h-screen bg-black">
     <Header/>
-    <div className="absolute">
-    <img src="https://assets.nflxext.com/ffe/siteui/vlv3/04ef06cc-5f81-4a8e-8db0-6430ba4af286/web/IN-en-20250224-TRIFECTA-perspective_3a9c67b5-1d1d-49be-8499-d179f6389935_small.jpg" alt='bg'/>
+    <div className="absolute inset-0">
+    <img 
+    className="w-full h-full object-cover"
+    src="https://assets.nflxext.com/ffe/siteui/vlv3/04ef06cc-5f81-4a8e-8db0-6430ba4af286/web/IN-en-20250224-TRIFECTA-perspective_3a9c67b5-1d1d-49be-8499-d179f6389935_small.jpg" alt='bg'/>
     </div>
   
-    <form onSubmit={(e)=>e.preventDefault()} className="max-w-md w-full sm:w-8/12 md:w-6/12 lg:w-3/12 absolute p-14 bg-black bg-opacity-80 my-20 mx-auto text-white right-0 left-0 rounded-lg shadow-lg">
+    <form onSubmit={(e)=>e.preventDefault()} className="max-w-md w-11/12 sm:w-8/12 md:w-6/12 lg:w-3/12 absolute p-6 sm:p-14 bg-black bg-opacity-80 my-20 mx-auto text-white right-0 left-0 rounded-lg shadow-lg">
   <h1 className="text-3xl font-bold py-4 ">
     {isSignInForm ?" Sign In" : "Sign Up"}
     </h1>
   
   {!isSignInForm &&(<input 
+  ref={name}
   type="text" placeholder="Name"
    className="border border-gray-300 bg-transparent p-4 my-2 w-full rounded-md" 
    />)
@@ -74,7 +104,7 @@ const Login = () => {
      ?"New to Netflix ? Sign up now." 
     : "Already a User ? Sign In Now"}
     </p>
-    <p className='text-sm my-2'>This page is protected by Google reCAPTCHA to ensure you're not a bot.</p>
+    <p className='text-sm my-2 text-gray-400'>This page is protected by Google reCAPTCHA to ensure you're not a bot.</p>
     {!showMore && (
           <button className="text-xs text-blue-500 cursor-pointer underline" onClick={() => setShowMore(true)}>
             Load More
@@ -82,9 +112,9 @@ const Login = () => {
         )}
     {showMore && (
       <p 
-      className='text-xs my-2'
+      className='text-xs my-2 text-gray-400'
       >
-        The information collected by Google reCAPTCHA is subject to the Google Privacy Policy and Terms of Service, and is used for providing, maintaining, and improving the reCAPTCHA service and for general security purposes (it is not used for personalised advertising by Google).
+        The information collected by Google reCAPTCHA is subject to the Google <span className='text-blue-600'>Privacy Policy</span>  and <span className='text-blue-600'>Terms of Service</span> , and is used for providing, maintaining, and improving the reCAPTCHA service and for general security purposes (it is not used for personalised advertising by Google).
         </p>
     )}
 </form>
---
+++ src/utils/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCGzOVAi-FTm82AtQsq-_zz7vCcPzqUiPU",
+  authDomain: "netflix-gpt-43eb6.firebaseapp.com",
+  projectId: "netflix-gpt-43eb6",
+  storageBucket: "netflix-gpt-43eb6.firebasestorage.app",
+  messagingSenderId: "989386712606",
+  appId: "1:989386712606:web:fb6be45ceedcff6375f00f",
+  measurementId: "G-FK3LVH0JCL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth();
